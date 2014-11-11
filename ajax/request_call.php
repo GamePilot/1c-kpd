1b2cf18d3ef363c68ec491d5f9e8e30b84870b18
Server <wmodin@mail.ru>
2014-11-11 13:09:22 +0300

    server update

---
+++ ajax/request_call.php
@@ -0,0 +1,22 @@
+<div class="modal">
+	<a href="#" class="close"><span>Close</span></a>
+	<p class="title">Заказать звонок</p>
+	<p>Оставьте заявку и мы свяжемся с Вами в указаное время.<br />Или позвоните нам сами: <strong>+7 (495) 640-00-93</strong></p>
+	<form>
+		<label>Представтесь, пожалуйста <span>*</span></label>
+		<input type="text" name="name">
+		<p class="info">Имеет смысл указать фамилию, если она потребуеться секретарю</p>
+
+		<label>Контактный телефон <span>*</span></label>
+		<input type="text" name="name" class="error" placeholder="Неверный формат номера">
+		<p class="info">Не забудьте код города и Ваш добавочный</p>
+
+		<label>Удобное время звонка <span>*</span></label>
+		<input type="text" name="name" placeholder="Неверный формат номера">
+		<p class="info">Например, «Сегодня с 12:00 до 13:00» или Прямо сейчас!</p>
+
+		<p class="info-b"><span>*</span> - Поля, обязательны к заполнению</p>
+
+		<a href="#" class="button">Отправить</a>
+	</form>
+</div>
