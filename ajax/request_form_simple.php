df22110396b0e85c37ea4d849db8e5a9d014c5af
Server <wmodin@mail.ru>
2014-11-18 16:30:13 +0300

    server update

---
+++ ajax/request_form_simple.php
@@ -0,0 +1,17 @@
+<div class="modal w270">
+	<!--<a href="#" class="close"><span>Close</span></a>-->
+	<form action="/ajax/form_send.php" method="post" data-type="online_call" enctype="application/x-www-form-urlencoded">
+		<label>Ф.И.О <span>*</span></label>
+		<input type="text" name="name">
+
+		<label>Телефон <span>*</span></label>
+		<input type="text" name="phone">
+
+		<label>Электронная почта <span>*</span></label>
+		<input type="text" name="email">
+
+		<p class="info-b no-br"><span>*</span> - Поля, обязательны к заполнению</p>
+
+		<a href="#" class="button modal">Отправить заявку</a>
+	</form>
+</div>
--- index.php
+++ index.php
@@ -106,83 +106,147 @@ $APPLICATION->SetTitle('1С КПД');
 <div class="container boxes vvedenie">
 	<div class="content">
 		<h1>ВНЕДРЕНИЕ</h1>
-		<p>1С-КПД является центром компетенции по документуобороту (1С:ЦКД) и занимем в нём</p>
-		<img src="/bitrix/templates/kpd/images/icon-cup.jpg" />I место
+		<img src="/bitrix/templates/kpd/images/line-3.png" class="line-3" />
+		<p class="small">1С-КПД является центром компетенции по документуобороту<br /> (1С:ЦКД) и занимем в нём</p>
+		<div class="icon-cup">
+			<img src="/bitrix/templates/kpd/images/icon-cup.jpg" />I место
+		</div>
 
 		<h1>Технологии внедрения «1С Документооборот»</h1>
 
 		<div class="content">
 			<div class="box b1">
 				<p class="title">Стандартное внедрение</p>
-				<p>Консультирование и настройка 1С:Документооборот по листам учета рабочего времени в соответствии с требованиями Заказчика. Стоимость определяется по фактически отработанным часам.</p>
+				<p class="text">Консультирование и настройка 1С:Документооборот по листам учета рабочего времени в соответствии с требованиями Заказчика. Стоимость определяется по фактически отработанным часам.</p>
 				<a href="/ajax/request_call.php" class="ajax button">Купить</a>
 				<div class="clr"></div>
 			</div>
 			<div class="box b2">
 				<p class="title">Модульное внедрение</p>
-				<p>Запуск типового функционала 1С:Документооборот по заранее определенным требованиям к модулям: НСИ, интеграция, входящие и исходящие, распорядительные документы, служебки, номенклатура дел, права доступа. Стоимость каждого модуля заранее определена. </p>
+				<p class="text">Запуск типового функционала 1С:Документооборот по заранее определенным требованиям к модулям: НСИ, интеграция, входящие и исходящие, распорядительные документы, служебки, номенклатура дел, права доступа. Стоимость каждого модуля заранее определена. </p>
 				<a href="/ajax/request_call.php" class="ajax button">Купить</a>
 				<div class="clr"></div>
 			</div>
 			<div class="box b3">
 				<p class="title">Корпоративное внедрение</p>
-				<p>Комплексное внедрение 1С:Документооборот с доработками. Стоимость определяется перед началом проекта после обследования процессов Заказчика. </p>
+				<p class="text">Комплексное внедрение 1С:Документооборот с доработками. Стоимость определяется перед началом проекта после обследования процессов Заказчика. </p>
 				<a href="/ajax/request_call.php" class="ajax button">Купить</a>
 				<div class="clr"></div>
 			</div>
-			<img src="/bitrix/templates/kpd/images/line.jpg" class="line" />
-			<div class="button">
-				<a href="/ajax/version.php" class="ajax button">Подробное сравнение</a>
-         </div>
 		</div>
-
 		<div class="inside">
-			<p>Расчет стоимости услуги</p>
-			<a href="/ajax/request_call.php" class="ajax button">Заказать звонок</a>
-		</div>
-	</div>
-</div>
-<hr>
-<!-- none -->
-<div class="container boxes saas">
-	<div class="content">
-		<h1>SaaS (аренда 1С:Документооборот)</h1>
-		<p>Виртуальный сервер, полностью настроенный для оптимальной работы с продуктами «1С:Документооборот 8» и готовый к немедленному использованию.</p>
-
-
-
-
-
-
-		<div class="right">
-			<p class="title">Расчет стоимости услуги</p>
-			<div class="inner">
-				<form action="/ajax/form_send.php" method="post" id="count_service_cost" data-type="calculator">     
-				<label>Колличество пользователей:</label><input value="1" type="text" name="user_count" ><br />
-				<label>Период оплаты (мес):</label>
-				<select id="pay_period" name="pay_period">
-                  			<option selected="" value="1">1</option>
-                  			<option value="3">3</option>
-                  			<option value="6">6</option>
-                  			<option value="9">9</option>
-                  			<option value="12">12</option>
-                  		</select>
-<br />
-				<label>Сумма ежемесячного платежа составит:</label><a class="service-cost">none</a><br />
-				<label>Сумма за период составит:</label><a class="period-cost">none</a>
-</form>
+			<div>
+				<p>Расчет стоимости услуги</p>
+				<a href="/ajax/request_call.php" class="ajax button">Заказать</a>
 			</div>
 		</div>
-
-
 	</div>
 </div>
 <hr>
 <!-- none -->
-
-
-
-
+<div class="container boxes obuchenie">
+	<div class="content">
+		<h1>ОБУЧЕНИЕ</h1>
+		<div class="form-inner">
+			<img src="/bitrix/templates/kpd/images/book.jpg" class="book" />
+			<div class="inner">
+				<p>Получите в подарок новую книгу <span>«1С:Документооборот. 200 вопросов и ответов»</span> или <span>просмотр записи мини-семинара по <br />1С:Документооборот для специалистов</span></p>
+				<div class="line"></div>
+				<div class="count">
+					<p>До окончания акции осталось:</p>
+					<div class="nummber margin">
+						<a href="#">2</a>
+						<span>Дня</span>
+					</div>
+					:
+					<div class="nummber">
+						<a href="#">4</a>
+						<span>Часов</span>
+					</div>
+					:
+					<div class="nummber">
+						<a href="#">10</a>
+						<span>Минут</span>
+					</div>
+					:
+					<div class="nummber">
+						<a href="#">5</a>
+						<span>Секунд</span>
+					</div>
+				</div>
+				<a href="/ajax/request_call.php" class="ajax button">Оставить заявку</a>
+				<div class="clr"></div>
+			</div>
+			<div class="clr"></div>
+		</div>
+		<h1>Формат обучения</h1>
+			<p class="text1">Курсы</p>
+			<p class="text2">Самостоятельное обучение</p>
+		<div class="box b1">
+			<p class="title">Мини-семинар по «1С:Документооборот» для специалистов</p>
+			<div class="inner bor">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-time-2.png" title="" alt="" />
+				</div>
+				<p>Длительность курса:<br /><span>4 академических часа</span></p>
+			</div>
+			<div class="inner bor">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-calendar-2.png" title="" alt="" />
+				</div>
+				<p>Ближайшие даты проведения:<br /><span>31 октября</span></p>
+			</div>
+			<div class="inner">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-money-2.png" title="" alt="" />
+				</div>
+				<p>Стоимость:<span>1 500 руб. - очно,<br />1 000 руб. - дистанционно,<br />2 000 руб - посмотреть в записи</span></p>
+			</div>
+			<p class="text">Курс по 1С:Документооборот для продвинутых слушателей. Знакомит со сложными настройками программы.</p>
+			<div class="bottom">
+				<p>Осталось мест - <span class="blue">10</span></p>
+				<a href="http://www.elektronnij-dokumentooborot.1c-kpd.ru/uchebnyi-centr/zapisatsa/form.php?kurs=106" target="_blank" class="button">Записаться</a>
+				<div class="clr"></div>
+			</div>
+		</div>
+		<div class="box b2">
+			<p class="title">Возможности прикладного решения «1С:Документооборот»</p>
+			<div class="inner bor">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-time-2.png" title="" alt="" />
+				</div>
+				<p>Длительность курса:<br /><span>3 дня</span></p>
+			</div>
+			<div class="inner bor">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-calendar-2.png" title="" alt="" />
+				</div>
+				<p>Ближайшие даты проведения:<br /><span>17 - 19 ноября</span></p>
+			</div>
+			<div class="inner">
+				<div class="img">
+					<img src="/bitrix/templates/kpd/images/icon-money-2.png" title="" alt="" />
+				</div>
+				<p>Стоимость:<span>10 000 руб.- очно, <br />8 000 руб.- дистанционно, <br />16 000 руб. - посмотреть в записи</span></p>
+			</div>
+			<p class="text">Базовый курс по 1С:Документооборот для пользователей и специалистов, начинающих работать с программой.</p>
+			<div class="bottom">
+				<p>Осталось мест - <span class="red">1</span></p>
+				<a href="http://www.elektronnij-dokumentooborot.1c-kpd.ru/uchebnyi-centr/zapisatsa/form.php?kurs=97" target="_blank" class="button">Записаться</a>
+				<div class="clr"></div>
+			</div>
+		</div>
+		<div class="box b3">
+			<p class="title"><strong>Наименование:</strong> <u>«1C:Электронное обучение. Работа с прикладным решением» «1C:Документооборот» (коробка / облако)</u></p>
+			<p class="title"><strong>Описание:</strong> Электронный аналог <u>очного курса</u><br /><strong>Возможности прикладного решения «1С:Документооборот»</strong><br /><br /><br /></p>
+			<p class="price">30 000 / 8 000 руб.</p>
+			<a href="/ajax/request_call.php" class="ajax button">Оставить заявку</a>
+			</div>
+		</div>
+	</div>
+</div>
+<hr>
+<!-- none -->
 <div class="container boxes form1">
 	<div class="content">
 		<p class="title">Получите в подарок новую книгу<br /> <span>«1С:Документооборот. 200 вопросов и ответов»</span> <br />или <span>просмотр записи мини-семинара</span><br /> по 1С:Документооборот для специалистов</p>
@@ -203,6 +267,108 @@ $APPLICATION->SetTitle('1С КПД');
 </div>
 <hr>
 <!-- none -->
+<div class="container boxes docstore">
+	<h1>DocStore</h1>
+		<img src="/bitrix/templates/kpd/images/line-2.png">
+		<p>Имея большой опыт внедрения и доработок 1С:Документооборот у разных клиентов,<br /> мы систематизируем лучшие практики и оформляем их как готовые решения.<br />Приобретая готовое решение, Вы получаете дополнительный функционал к СЭД.</p>
+		<div class="content">
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1s-kpd-sinkhronizatsiya-kalendarey-s-mobilnymi-ustroystvami/" target="_blank"><span>1С-КПД: Синхронизация<br />календарей<br />с мобильными <br />устройствами</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/kniga-1s-dokumentooborot-200-voprosov-i-otvetov/" target="_blank"><span>Книга «1С:Документооборот.<br />200 вопросов и ответов»</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1s-kpd-besshovnaya-integratsiya-finansovyh-dokumentov/" target="_blank"><span>1С-КПД: Бесшовная интеграция<br />финансовых документов</span></a>
+			</div>
+			<div class="box no-mr">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1s-kpd-dok/" target="_blank"><span>1С-КПД iDecide: Рабочее место<br />руководителя на iPad</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/elektronnyy-uchebnyy-kurs-rabota-s-prikladnym-resheniem-1s-dokumentooborot-8/" target="_blank"><span>Электронный учебный курс «Работа с прикладным решением «1С:Документооборот»</span></a>     
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/mobilnyy-polzovatel/" target="_blank"><span>1С-КПД: Мобильный пользователь</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/moduli-integratsii/" target="_blank"><span>1С-КПД: Интеграция общих справочников</span></a>
+			</div>
+			<div class="box no-mr">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/dokumentooborot_v_oblake/" target="_blank"><span>1С:Документооборот в облаке</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1c-kpd-pechat-konvertov/" target="_blank"><span>1C-КПД: Печать конвертов</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1s-kpd-proverka-orfografii/" target="_blank"><span>1С-КПД: Проверка орфографии</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1s-kpd-sms-opoveshchenie/" target="_blank"><span>1С-КПД: СМС-оповещение</span></a>
+			</div>
+			<div class="box no-mr">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/komplekt-normativnykh-dokumentov-1s-dokumentooborot-korp-/" target="_blank"><span>Комплект нормативных документов «1С:Документооборот КОРП»</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/kopmlekt-normativnykh-dokumentov-1s-dokumentooborot-8-prof/" target="_blank"><span>Копмлект нормативных документов «1С:Документооборот ПРОФ»</span></a>
+			</div>
+			<div class="box">
+				<a href="http://elektronnij-dokumentooborot.1c-kpd.ru/products/docstore/1c-kpd-gruppovaya-pechat/" target="_blank"><span>1C-КПД: Групповая печать</span></a>
+			</div>
+		</div>
+</div>
+<hr>
+<!-- none -->
+<div class="container boxes docstore saas">
+	<h1>SaaS (аренда 1С:Документооборот)</h1>
+	<img src="/bitrix/templates/kpd/images/line-2.png">
+	<p>Виртуальный сервер, полностью настроенный для оптимальной работы с продуктами<br /> «1С:Документооборот» и готовый к немедленному использованию.</p>
+	<div class="inner">
+		<div class="left">
+			<p>Преимущества:</p>
+			<ul>
+				<li><span>1</span><p>установленное и готовое к использованию решение 1С:Документооборот КОРП;</p></li>
+				<li><span>2</span><p>полное техническое обслуживание на современном серверном оборудовании;</p></li>
+				<li><span>3</span><p>автоматическое архивирование базы данных;</p></li>
+				<li><span>4</span><p>доступ к программе из любой точки, где есть интернет;</p></li>
+				<li><span>5</span><p>экономия денег на покупке дорогостоящего софта и оборудования.</p></li>
+			</ul>
+		</div>
+		<div class="right">
+			<p>Расчет стоимости услуги</p>
+			<form action="/ajax/form_send.php" method="post" id="count_service_cost" data-type="calculator">     
+				<div class="row">
+					<label>Колличество пользователей:</label><input value="1" type="text" name="user_count" >
+				</div>
+				<div class="row">
+					<label>Период оплаты (мес):</label>
+					<select id="pay_period" name="pay_period">
+						<option selected="" value="1">1</option>
+						<option value="3">3</option>
+						<option value="6">6</option>
+						<option value="9">9</option>
+						<option value="12">12</option>
+					</select>
+				</div>
+				<div class="line"></div>
+				<div class="row">
+					<label>Сумма ежемесячного платежа составит:</label><a class="service-cost">none</a><span>Руб.</span>
+				</div>
+				<div class="row">
+					<label>Сумма за период составит:</label><a class="period-cost">none</a><span>Руб.</span>
+				</div>
+			</form>
+		</div>
+		<div class="clr"></div>
+	</div>
+	<div class="expert">
+		<img src="/bitrix/templates/kpd/images/bg-expert.jpg" />
+		<p>Документооборот в облаке— это полноценное решение 1С:Документооборот, которое включает все возможности системы за меньшие деньги.</p>
+		<p>Хранить документы в облаке — абсолютно безопасно, каналы связи защищены по протоколу https, доступ к серверам хранения данных защищен и ограничен разграничением прав доступа и персональными паролями.</p>
+		<p>Работа с системой возможна из любого места где есть интернет: из офиса, из дома, в командировке и отпуске. 1С:Документооборот работает на мобильных устройствах, таких как iPad.</p>
+	</div>
+</div>
+<hr>
+<!-- none -->
 <div class="container boxes form2">
 	<div class="content">
 		<p class="title">Получите в подарок</p>
@@ -212,6 +378,104 @@ $APPLICATION->SetTitle('1С КПД');
 			<input type="email" name="email" class="email" placeholder="E-mail">
 			<a href="#" class="button form">Оставить заявку</a>
 		</form>
+	</div>
+</div>
+<hr>
+<!-- none -->
+<div class="container boxes soprov">
+	<div class="content">
+		<h1>Сопровождение (техподдержка)</h1>
+		<p class="text">Преимущества регулярного обслуживания</p>
+		<div class="box">
+			<img src="/bitrix/templates/kpd/images/icon-time.png" title="" alt="" />
+			<p>Стоимость часа при абонентском обслуживании дешевле, чем при разовых обращениях</p>
+		</div>
+		<div class="box">
+			<img src="/bitrix/templates/kpd/images/icon-service.png" title="" alt="" />
+			<p>Включены дополнительные cервисы</p>
+		</div>
+		<div class="box">
+			<img src="/bitrix/templates/kpd/images/icon-top.png" title="" alt="" />
+			<p>Выгодные тарифы обновления нетиповых  конфигураций</p>
+		</div>
+		<p class="text">Преимущества регулярного обслуживания</p>
+		<div class="box2">
+			<ul>
+				<li><span>1</span><p>Скидка на курсы в Учебном центре 1С-КПД – 20%</p></li>
+				<li><span>2</span><p>Книга «1С:Документооборот 200 вопросов и ответов» в подарок</p></li>
+				<li><span>3</span><p>Индивидуальная подписка на новости об изменениях в продукте, законода-<br />тельстве и отраслевых мероприятиях</p></li>
+				<li><span>4</span><p>Бесплатный доступ к электронному<br /> курсу «1С:Электронное обучение»</p></li>
+				<li><span>5</span><p>Бесплатный доступ к электронному<br /> курсу «Методология внедрения 1С:Документооборот»</p></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<hr>
+<!-- none -->
+<div class="container boxes tab">
+	<div class="content">
+		<h1>Таблица тарифов</h1>
+		<table>
+			<thead>
+				<tr>
+					<td class="left">Тарифы</td>
+					<td class="width">Онлайн</td>
+					<td class="width">Базовый</td>
+					<td class="width">Стандарт</td>
+					<td class="width">Проф</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="bg">
+					<td class="left">
+						<strong>Удаленная техническая поддержка</strong> Весь спектр услуг: от решения проблемы в режиме «вопрос-ответ» до удаленного подключения к рабочему столу пользователя. Кроме услуг по програмированию.
+					</td>
+					<td>4 часа</td>
+					<td>1 часа</td>
+					<td>4 часа</td>
+					<td>4 часа</td>
+				</tr>
+				<tr>
+					<td class="left"><strong>Обновление типовых конфигураций</strong> В рамках договора информационно-технического сопровождения</td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+				</tr>
+				<tr class="bg">
+					<td class="left"><strong>Работы специалиста</strong>Все виды услуг: от решения проблемы в режиме «вопрос-ответ» до удаленного подключения к рабочему столу пользователя. Кроме услуг по програмированию.</td>
+					<td><img src="/bitrix/templates/kpd/images/no-check.png" /></td>
+					<td>4 часа</td>
+					<td>8 часа</td>
+					<td>16 часа</td>
+				</tr>
+				<tr>
+					<td class="left"><strong>Личный консультант</strong>В рамках договора информационно-технического сопровождения</td>
+					<td><img src="/bitrix/templates/kpd/images/no-check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/no-check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+					<td><img src="/bitrix/templates/kpd/images/check.png" /></td>
+				</tr>
+				<tr class="bg">
+					<td class="left"><strong>Стоимость за месяц</strong></td>
+					<td class="strong">4 500 руб.</td>
+					<td class="strong">9 500 руб.</td>
+					<td class="strong">19 500 руб.</td>
+					<td class="strong">39 500 руб.</td>
+				</tr>
+				<tr>
+					<td class="left"><strong>Обновление нетиповых конфигураций</strong>В рамках договора информационно-технического сопровождения</td>
+					<td colspan="4" class="strong">4 900 руб./ мес. или 42 000 руб./ год.</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td class="button"><a href="/ajax/request_form_simple.php" class="ajax button">Купить</a></td>
+					<td class="button"><a href="/ajax/request_form_simple.php" class="ajax button">Купить</a></td>
+					<td class="button"><a href="/ajax/request_form_simple.php" class="ajax button">Купить</a></td>
+					<td class="button"><a href="/ajax/request_form_simple.php" class="ajax button">Купить</a></td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </div>
 <hr>
@@ -235,7 +499,7 @@ $APPLICATION->SetTitle('1С КПД');
 		</form>
 	</div>
 </div>
-
+<hr>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
